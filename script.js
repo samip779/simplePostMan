aade9ae40cc6add19bfbdd93204f78c558ccb24b
samip779 <samipluitel28@gmail.com>
2022-09-26 07:21:43 +0545

    axios request haldled

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
+const form = document.querySelector("[data-form]");
 const queryParamsContainer = document.querySelector("[data-query-params]");
 const requestHeadersContainer = document.querySelector(
   "[data-request-headers]"
@@ -22,10 +24,35 @@ document
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(keyValuePairsToObjects(queryParamsContainer));
+  console.log(keyValuePairsToObjects(requestHeadersContainer));
+  axios({
+    url: document.querySelector("[data-url]").value,
+    method: document.querySelector("[data-method]").value,
+    params: keyValuePairsToObjects(queryParamsContainer),
+    headers: keyValuePairsToObjects(requestHeadersContainer),
+  }).then((response) => {
+    console.log(response.data);
+  });
+});
+
 function createKeyValuePair() {
   const element = keyValueTemplate.content.cloneNode(true);
   element.querySelector("[data-remove-btn]").addEventListener("click", (e) => {
     e.target.closest("[data-key-value-pair]").remove();
   });
   return element;
+}
+
+function keyValuePairsToObjects(container) {
+  const pairs = container.querySelectorAll("[data-key-value-pair]");
+  return [...pairs].reduce((data, pair) => {
+    const key = pair.querySelector("[data-key]").value;
+    const value = pair.querySelector("[data-value]").value;
+
+    if (key == "") return data;
+    return { ...data, [key]: value };
+  }, {});
 }
